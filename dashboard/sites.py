a2b5e1afd41b505560b0a30ecbbcca969d32df93
fuqiang <fuqiang.work@foxmail.com>
2023-11-20 16:46:56 +0800

    🎨-【html 代码优化 】

--- dashboard/sites.py
+++ dashboard/sites.py
@@ -1,7 +1,5 @@
 from django.urls import path
 
-from dashboard import views
-
 class DashboardSite:
     def __init__(self):
         self._registry = {}
@@ -18,14 +16,6 @@ class DashboardSite:
 
     def get_registered_dashboards(self):
         return self._registry
-
-    def get_urls(self):
-        urlpatterns = []
-        for name, dashboard_cls in self._registry.items():
-            urlpatterns.append(
-                path(f'{name}/', views.dashboard_view, kwargs={'dashboard_name': name}, name=f'dashboard_{name}')
-            )
-        return urlpatterns
 
 
 dashboard_site = DashboardSite()
