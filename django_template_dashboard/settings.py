f64c6acef810f61b03a0d78d1c73c6d074afe9dd
fuqiang <fuqiang.work@foxmail.com>
2023-11-20 16:49:45 +0800

    🎨-【html 代码优化 】

--- django_template_dashboard/settings.py
+++ django_template_dashboard/settings.py
@@ -41,6 +41,10 @@ INSTALLED_APPS += [
     'dashboard'
 ]
 
+INSTALLED_APPS += [
+    'tests'
+]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
