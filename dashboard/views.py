e4c0629313fe27479cf756c79d57ca6b6ea5c18d
fuqiang <fuqiang.work@foxmail.com>
2023-11-20 11:25:07 +0800

    🎨-【html 代码优化 】

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render, redirect
 from django.contrib.auth.views import LoginView, PasswordResetView, PasswordChangeView, PasswordResetConfirmView
 from dashboard.forms import RegistrationForm, LoginForm, UserPasswordResetForm, UserSetPasswordForm, \
@@ -7,6 +8,20 @@ from django.contrib.auth import logout
 # 在 views.py 文件中
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import View
+
+from django.shortcuts import render
+
+from . import registry
+
+
+def dashboard_view(request, dashboard_name):
+    dashboard_cls = registry.dashboard_registry.get_registered_dashboards().get(dashboard_name)
+    if dashboard_cls is None:
+        raise Http404(f"Dashboard '{dashboard_name}' not found")
+
+    dashboard = dashboard_cls()
+    context = dashboard.get_context_data()
+    return render(request, 'dashboard/index.html', context)
 
 
 class DashboardView(LoginRequiredMixin, View):
